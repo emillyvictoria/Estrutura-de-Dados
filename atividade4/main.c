6454837697e34f2fb9d02f1ccfeb28d542c4d66c
emillyvictoria <emillyvsp02@gmail.com>
2023-09-18 09:37:25 -0300

    ajustando erros do merge

---
+++ atividade4/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "bubble_sort.h"
+
+void imprimir_arr(int arr[], int n){
+    printf("\n");
+    for(int i =0; i<n; i++){
+        printf("%d - ",arr[i]);
+    }
+     printf("\n");
+}
+
+int main(int argc, char* argv[]){
+    int arr_inteiros[argc]; 
+
+    for(int i=0; i < argc; i++){
+
+       arr_inteiros[i] = atoi(argv[i+1]);
+    }
+
+    
+    int n = sizeof(arr_inteiros)/sizeof(int);
+
+    imprimir_arr(arr_inteiros, n);
+
+    bubble_sort(arr_inteiros, n);
+
+    imprimir_arr(arr_inteiros, n);
+
+    exit(0);
+}
