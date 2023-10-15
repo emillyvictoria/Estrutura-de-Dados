7fe782a940b11588a02e2cfaa9ac6b33c6724f18
emillyvictoria <emillyvsp02@gmail.com>
2023-10-15 15:00:29 -0300

    atvd8

---
+++ atividade8/lista.c
@@ -0,0 +1,121 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lista.h"
+
+No* no(char valor, No* proximo_no){
+    No* no = malloc(sizeof(No));
+    no->valor = valor;
+    no->proximo_no = proximo_no;
+    return no;
+}
+
+void lista_inserir_no(No* L, No* no){
+    if(L != NULL){
+        if(L->proximo_no == NULL){
+            L->proximo_no = no;
+        }
+        else{
+            lista_inserir_no(L->proximo_no, no);
+        }
+    }
+}
+
+void lista_imprimir(No* L){
+    if(L != NULL){
+        printf("%c ", L->valor);
+        lista_imprimir(L->proximo_no);
+    }
+}
+
+int lista_quantidade_nos(No* L){
+    if(L != NULL){
+        return 1 + lista_quantidade_nos(L->proximo_no);
+    }
+    return 0;
+}
+
+No* lista_copiar(No* L){
+    if(L != NULL){
+        return no(L->valor, lista_copiar(L->proximo_no));
+    }
+    return NULL;
+}
+
+void lista_concatenar(No* L, No* Lc){
+    lista_inserir_no(L, Lc);
+}
+
+void lista_liberar(No* L){
+    if(L != NULL){
+        lista_liberar(L->proximo_no);
+        free(L);
+    }
+
+    int lista_verificar_existencia(No* L, char valor_busca) {
+    while (L != NULL) {
+        if (L->valor == valor_busca) {
+            return 1; // Valor encontrado
+        }
+        L = L->proximo_no;
+    }
+    return 0; // Valor não encontrado
+}
+
+int lista_verificar_ocorrencias(No* L, char valor_busca) {
+    int ocorrencias = 0;
+    while (L != NULL) {
+        if (L->valor == valor_busca) {
+            ocorrencias++;
+        }
+        L = L->proximo_no;
+    }
+    return ocorrencias;
+}
+
+void lista_imprimir_inversa(No* L) {
+    if (L != NULL) {
+        lista_imprimir_inversa(L->proximo_no);
+        printf("%c ", L->valor);
+    }
+}
+
+void lista_inserir_no_i(No* L, int i) {
+    if (i == 0) {
+        // Inserir no início da lista
+        No* novo_no = no('N', L);
+        L = novo_no;
+    } else if (i > 0 && L != NULL) {
+        lista_inserir_no_i(L->proximo_no, i - 1);
+    } else {
+        printf("Índice inválido para inserção.\n");
+    }
+}
+
+void lista_remover_no_i(No* L, int i) {
+    if (i == 0 && L != NULL) {
+        No* no_a_remover = L->proximo_no;
+        if (no_a_remover != NULL) {
+            L->proximo_no = no_a_remover->proximo_no;
+            free(no_a_remover);
+        } else {
+            printf("Índice inválido para remoção.\n");
+        }
+    } else if (i > 0 && L != NULL) {
+        lista_remover_no_i(L->proximo_no, i - 1);
+    } else {
+        printf("Índice inválido para remoção.\n");
+    }
+}
+
+void lista_remover_no(No* L, char valor_busca) {
+    while (L != NULL && L->proximo_no != NULL) {
+        if (L->proximo_no->valor == valor_busca) {
+            No* no_a_remover = L->proximo_no;
+            L->proximo_no = no_a_remover->proximo_no;
+            free(no_a_remover);
+        } else {
+            L = L->proximo_no;
+        }
+    }
+}
+}
---
+++ atividade8/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lista.h"
+
+int main(int argc, char* argv[]){
+    No* n0 = no('C', NULL);
+    No* n1 = no('E', NULL);
+    No* n2 = no('A', no('D', no('B', no('X', no('Z', NULL)))));
+    No* n3 = no('U', NULL);
+    
+    lista_inserir_no(n0, n2);
+    lista_inserir_no(n0, n1);
+    lista_inserir_no(n0, n3);
+
+    lista_imprimir(n0);
+    printf("\nQuantidade de nós: %d", lista_quantidade_nos(n0));
+
+    char valor_busca = 'D';
+    printf("\nVerificar existência de '%c': %d", valor_busca, lista_verificar_existencia(n0, valor_busca));
+
+    valor_busca = 'C';
+    printf("\nOcorrências de '%c': %d", valor_busca, lista_verificar_ocorrencias(n0, valor_busca));
+
+    printf("\nLista em ordem inversa: ");
+    lista_imprimir_inversa(n0);
+
+    int i = 2;
+    lista_inserir_no_i(n0, i);
+
+    i = 5;
+    lista_remover_no_i(n0, i);
+
+    valor_busca = 'C';
+    lista_remover_no(n0, valor_busca);
+
+    printf("\nLista após operações:\n");
+    lista_imprimir(n0);
+    printf("\n");
+
+    lista_liberar(n0);
+    n0 = NULL;
+
+    return 0;
+}
